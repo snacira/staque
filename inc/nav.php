49bf620f1f459240491783fd7171d932d73d5c4d
nacira <s.nacira@gmail.com>
2014-10-17 16:34:15 +0200

    add files

--- inc/nav.php
+++ inc/nav.php
@@ -1,6 +1,8 @@
 <header>
 	<div class="connexion">
-		<a href="index.php"><img id="logo" src="img/logo.png"></a>
+		<a href="index.php" id="logo">
+			<img src="img/logo.png" alt="logo Staque.fr" width="67px" height="30px">
+		</a>
 		<div id="login">
 			<a href="login.php">Connexion</a> / 
 			<a href="register.php">Crée un compte</a>
@@ -10,11 +12,10 @@
 		<div class="liens">
 			<a href="question_form.php">Poser une question</a>
 			<a href="profil_list.php">Utilisateurs</a>
-			<a href="">Mots-clé</a>
-			
-			<div id="recherche" >
-				<input class="loupe" type="text" placeholder="Recherche">
-			</div>
+			<a href="">Mots-clé</a>	
+		</div>
+		<div id="recherche" >
+			<input class="loupe" type="text" placeholder="Recherche">
 		</div>
 	</nav>
 </header>
