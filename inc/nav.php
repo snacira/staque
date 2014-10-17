94cfdd14b22558429f2a40152c0c0ae7b4edd319
marcelo <mclopds@gmail.com>
2014-10-17 14:18:50 +0200

    nav

---
+++ inc/nav.php
@@ -0,0 +1,14 @@
+<header>
+	<div class="connexion">
+		<img src="">
+		<a href="inc/login_form.php">Connexion</a> / 
+		<a href="inc/register_form.php">Crée un compte</a>
+	</div>
+	<nav class="navigation">
+		<a href="inc/answer_form.php">Posser une question</a>
+		<a href="profil_list.php">Utilisateurs</a>
+		<a href="">Mots-clé</a>
+		
+		<input type="text" placeholder="Recherche">
+	</nav>
+</header>
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php 
+	include("inc/header.php");
+	include("inc/nav.php");
+	include("question_list.php");
+	include("inc/footer.php")
+?>
