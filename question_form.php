5d7c7e88636d5e0ac3dc56bf8f6afca97be6cb3f
marcelo <mclopds@gmail.com>
2014-10-21 15:36:16 +0200

    refreshForm

--- question_form.php
+++ question_form.php
@@ -5,6 +5,7 @@
 	include("db.php");
 	include("functions.php");
 
+	$errors = array();
 	if (!empty($_POST)){
 		$maquestion = addquestion();
 	}
@@ -19,18 +20,28 @@
 	
 	<form id="formulairedequestion" method="POST">
 			<label>Titre
-				<input class="intext" type="text" name="title" placeholder="Entré votre titre">
+				<input class="intext" type="text" name="title" placeholder="Entré votre titre" value="<?php if (!empty($_POST['title'])){ echo $_POST['title'] ;}?>">
 			</label>
 			
 			<label class="questionarea">
-				<textarea name="content" placeholder="Poser votre question"></textarea>
+				<textarea name="content" placeholder="Poser votre question"><?php if (!empty($_POST['content'])){ echo $_POST['content'] ;}?></textarea>
 			</label>
 			
 			<label id="tags">Mots-clé
 				<input class="intext" type="text"placeholder="Choisiez vos mots clé">
 			</label>
+
+			<?php 
+		if (!empty($errors)){
+			echo '<ul class="errors">';
+			foreach($errors as $error){
+				echo '<li>'.$error.'</li>';
+			}
+			echo '</ul>';
+		}
+	?>
 			
-			<label class="submit"><input type="submit"></label>
+			<label class="submit"><input type="submit" value="Posser la question"></label>
 	</form>
 
 </section>
