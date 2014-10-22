67f4b7bc6e1627cef538e5fd845180b31c956831
marcelo <mclopds@gmail.com>
2014-10-22 17:28:07 +0200

    deco

---
+++ delete_profil.php
@@ -0,0 +1,16 @@
+<?php 
+
+	$id = $_GET['user'];
+
+	include("db.php");
+
+	$sql ="DELETE from user WHERE id = :id";
+
+	$stmt = $dbh->prepare($sql);
+
+	$stmt->bindValue(":id", $id);
+
+	$stmt->execute();
+
+	header("location: index.php")
+	?>
